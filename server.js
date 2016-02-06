cd1aa59d32e434ef40952833ef3536f0d04e4bb6
DJO3 <dev@pytools.org>
2016-02-06 15:08:45 -0500

    preliminary server setup

---
+++ server.js
@@ -0,0 +1,59 @@
+var express = require('express')
+var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
+var morgan = require('morgan')
+
+/* models */
+require('./app/models/user')
+mongoose.connect('mongodb://localhost:27017/mean-machine')
+
+/* routes */
+var apiRoute = require('./app/routes/api')
+
+/* init app */
+var app = express()
+
+/* logging */
+app.use(morgan('dev'))
+
+/* access data from POSTs */
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+/* register routes */
+app.use('/api', apiRoute)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+/* error handling */
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
+    res.json({
+      message: err.message,
+      error: err
+    })
+  })
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500)
+  res.json({
+    message: err.message,
+    error: {}
+  })
+})
+
+/* start server */
+var port = process.env.PORT || 8080
+app.listen(port)
