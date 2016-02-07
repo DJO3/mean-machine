ceff152ddc1eebecd1b72dce5ccb878070eefea5
DJO3 <dev@pytools.org>
2016-02-06 19:05:48 -0500

    add CORS handling

--- server.js
+++ server.js
@@ -20,6 +20,15 @@ app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+/* handle CORS */
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+  res.setHeader('Access-Control-Allow-Headers',
+                'X-Requested-With,content-type Authorization')
+  next()
+})
+
 /* register routes */
 app.use('/api', apiRoute)
 
