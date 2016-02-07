fd737b0330149fa96b00ee6614652ecbdd070cc9
DJO3 <dev@pytools.org>
2016-02-06 21:14:11 -0500

    added delete user, updated error handling

--- app/routes/api.js
+++ app/routes/api.js
@@ -41,6 +41,8 @@ router.get('/users/:user_id', function (req, res, next) {
   User.findById(req.params.user_id, function (err, user) {
     if (err) {
       return next(err)
+    } else if (!user) {
+      return res.status(404).json({ error: 'User not found!' })
     }
 
     res.json(user)
@@ -52,6 +54,8 @@ router.put('/users/:user_id', function (req, res, next) {
   User.findById(req.params.user_id, function (err, user) {
     if (err) {
       return next(err)
+    } else if (!user) {
+      return res.status(404).json({ error: 'User not found!' })
     }
 
     if (req.body.name) {
@@ -71,6 +75,20 @@ router.put('/users/:user_id', function (req, res, next) {
 
       res.json({ message: 'User information successfully updated!' })
     })
+  })
+})
+
+// delete a user
+router.delete('/users/:user_id', function (req, res, next) {
+  var deleteUser = { _id: req.params.user_id }
+  User.remove(deleteUser, function (err, user) {
+    if (err) {
+      return next(err)
+    } else if (!user) {
+      return res.status(404).json({ error: 'User not found!' })
+    }
+
+    res.json({ message: 'User successfully deleted' })
   })
 })
 
